4c0dbb17bc5ccaa6b432ddd3c9d033004bd70207
Sura Jeon <surajeon@gmail.com>
2021-04-12 11:01:53 +0000

    add all

--- ice-cream.js
+++ ice-cream.js
@@ -3,4 +3,6 @@ console.log(iceCreamFlavours);
 
 iceCreamFlavours.push("root beer");
 console.log(iceCreamFlavours);
-
+console.log(iceCreamFlavours[0]);
+console.log(iceCreamFlavours[5]);
+console.log(iceCreamFlavours.length);
